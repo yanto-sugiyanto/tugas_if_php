f36eaa38a14c45b37db81d815558d02345023c4b
yanto-sugiyanto <71365660+yanto-sugiyanto@users.noreply.github.com>
2020-10-08 11:42:19 +0700

    Add files via upload

---
+++ tugas_if_php.php
@@ -0,0 +1,20 @@
+<?php
+
+$a=50;
+$b=7; // Inisialisasi variabel bil dengan nilai 10
+
+if ($a % 2 ==0){ //Kondisi
+	
+    echo "$a Merupakan Bilangan Genap"; //Kondisi true
+}else {
+    echo "$a Merupakan Bilangan Ganjil"; //Kondisi false
+}
+
+if ($b % 2 ==0): //Kondisi
+    echo "<br> $b Merupakan Bilangan Genap"; //Kondisi true
+else :
+    echo "<br> $b Merupakan Bilangan Ganjil"; //Kondisi false
+
+endif
+
+?>
